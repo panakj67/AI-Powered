d84aeb85cd4348f7f23499b0bd87c791c35463e3
Pankaj Soni <pankajsony9304@gmail.com>
2026-02-24 20:33:49 +0530

    UI Refined

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose"
 const connectDb=async ()=>{
     try {
         await mongoose.connect(process.env.MONGODB_URL)
-        console.log("Connected to DB")
+        console.log("Connected to DB");
+        
     } catch (error) {
         console.log(error)
     }
